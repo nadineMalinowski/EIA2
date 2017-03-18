32166bbbe47d0cf526c3b465c43f5b69d3960a20
Nadin <Nadin@192.168.1.8>
2017-03-18 14:15:01 +0100

    Aufgabe 0

---
+++ Aufgabe 0/Aufgabe 0.js
@@ -0,0 +1,10 @@
+//Aufgabe: 0-Arbeitsf�higkeit
+//Name: Nadine Malinowski
+//Datum: 18.03.2017
+//Matrikel: 254763
+//Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
+document.addEventListener('DOMContentLoaded', function () {
+    var eingabe = prompt("Bitte geben Sie ihren Namen ein :)");
+    document.body.innerHTML = "Herzlich Willkomen " + eingabe + "!";
+});
+//# sourceMappingURL=Aufgabe 0.js.map
---
+++ Aufgabe 0/Aufgabe 0.ts
@@ -0,0 +1,14 @@
+
+//Aufgabe: 0-Arbeitsfähigkeit
+//Name: Nadine Malinowski
+//Datum: 18.03.2017
+//Matrikel: 254763
+//Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    
+    var eingabe = prompt("Bitte geben Sie ihren Namen ein :)");
+    
+    document.body.innerHTML = "Herzlich Willkomen " + eingabe + "!";
+});

ccc1a4d42ebb771819eb6520bbf4c334df061947
Nadin <Nadin@192.168.1.9>
2017-03-25 20:26:46 +0100

    Aktualisierung

--- Aufgabe 0/Aufgabe 0.js
+++ Aufgabe 0/Aufgabe 0.js
@@ -4,7 +4,7 @@
 //Matrikel: 254763
 //Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
 document.addEventListener('DOMContentLoaded', function () {
-    var eingabe;
+    var eingabe: string;
     var eingabe = prompt("Bitte geben Sie ihren Namen ein :)");
     document.body.innerHTML = "Herzlich Willkomen " + eingabe + "!";
 });

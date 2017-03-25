1af5a6bbd256bad6dc98ea046b9171c681e19159
Nadin <Nadin@192.168.1.9>
2017-03-25 20:51:08 +0100

    Aktualisierung

---
+++ Aufgabe 0/Aufgabe0.js
@@ -0,0 +1,11 @@
+//Aufgabe: 0-Arbeitsf�higkeit
+//Name: Nadine Malinowski
+//Datum: 18.03.2017
+//Matrikel: 254763
+//Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
+document.addEventListener('DOMContentLoaded', function () {
+    var eingabe;
+    var eingabe = prompt("Bitte geben Sie ihren Namen ein :)");
+    document.body.innerHTML = "Herzlich Willkomen " + eingabe + "!";
+});
+//# sourceMappingURL=Aufgabe0.js.map

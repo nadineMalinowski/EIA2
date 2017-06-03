6492c0de1cd438166aa6892c8bdade1732f6bb66
Nadin <Nadin@192.168.1.7>
2017-06-03 20:08:03 +0200

    CodeDelloro

--- Aufgabe8/CodeDelloro/Circle.js
+++ Aufgabe8/CodeDelloro/Circle.js
@@ -1,1 +1,34 @@
+var L8_Inheritance;
+(function (L8_Inheritance) {
+    class Circle extends L8_Inheritance.MovingShape {
+        constructor(_x, _y) {
+            super(_x, _y);
+            console.log("Create circle");
+            this.speed = 0.1;
+            this.setRandomPosition();
+            this.setRandomTargetPosition();
+        }
+        setRandomTargetPosition() {
+            this.xTarget = Math.random() * L8_Inheritance.crc2.canvas.width;
+            this.yTarget = Math.random() * L8_Inheritance.crc2.canvas.height;
+        }
+        draw() {
+            L8_Inheritance.crc2.fillStyle = this.color;
+            L8_Inheritance.crc2.beginPath();
+            L8_Inheritance.crc2.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+            L8_Inheritance.crc2.fill();
+        }
+        move() {
+            let xDiff = this.xTarget - this.x;
+            let yDiff = this.yTarget - this.y;
+            if (Math.abs(xDiff) < 1 && Math.abs(yDiff) < 1)
+                this.setRandomTargetPosition();
+            else {
+                this.x += xDiff * this.speed;
+                this.y += yDiff * this.speed;
+            }
+        }
+    }
+    L8_Inheritance.Circle = Circle;
+})(L8_Inheritance || (L8_Inheritance = {}));
 //# sourceMappingURL=Circle.js.map
